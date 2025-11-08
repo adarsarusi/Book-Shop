20dec89cada1175fea6ae0f03b7067f4dd6972c5
Adar <adar.sarusi@gmail.com>
2025-11-08 12:29:47 +0200

    Removing hard coded data and adding gBooks dynamically

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,5 +1,19 @@
-function onInit(){
+function onInit() {
     renderBooks()
 }
 
-function renderBooks(){}
+function renderBooks() {
+    const books = getBooks()
+    const strHTMLs = books.map(book => `
+        <tr>
+            <td>${book.name}</td>
+            <td>${book.price}</td>
+            <td><button>Read</button>
+            <button>Update</button>
+            <button>Delete</button></td>
+        </tr>
+        `)
+
+    const elTable = document.querySelector('.book-shop')
+    elTable.innerHTML += strHTMLs.join('')
+}
